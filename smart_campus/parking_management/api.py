15a5c23eafa35c2dc46896d7c8b20e08b9414927
ashboi005 <125044617+ashboi005@users.noreply.github.com>
2024-11-15 23:48:32 +0530

    added parking_management app

--- smart_campus/parking_management/api.py
+++ smart_campus/parking_management/api.py
@@ -1,3 +1,4 @@
+# parking_report/api.py
 from ninja import NinjaAPI
 from .models import ParkingData
 from .schemas import ParkingDataSchema, CreateParkingDataSchema
@@ -5,3 +6,42 @@ from typing import List
 from django.shortcuts import get_object_or_404
 
 app2 = NinjaAPI(urls_namespace="parking_management")
+
+@app2.post("/add_or_update", response=ParkingDataSchema)
+def add_or_update_parking_data(request, payload: CreateParkingDataSchema):
+    """Add or update parking data entry based on sensor_id."""
+
+    # Get all records for the given sensor_id, ordered by timestamp, skipping the latest one
+    old_records = ParkingData.objects.filter(sensor_id=payload.sensor_id).order_by('-timestamp')[1:]
+    
+    # Delete old records by their IDs
+    old_record_ids = old_records.values_list('id', flat=True)
+    ParkingData.objects.filter(id__in=old_record_ids).delete()
+
+    # Update or create the latest record
+    parking_data, created = ParkingData.objects.update_or_create(
+        sensor_id=payload.sensor_id,
+        defaults={
+            "sensor_location": payload.sensor_location,
+            "parking_status": payload.parking_status,
+        }
+    )
+    print(f"status of {payload.sensor_id} changed to {payload.parking_status} at {payload.sensor_location}")
+    return parking_data
+
+
+@app2.get("/list", response=List[ParkingDataSchema])
+def list_parking_data(request):
+    """Retrieve a list of all parking data entries."""
+    return ParkingData.objects.all()
+
+
+@app2.get("/get/{sensor_id}", response=ParkingDataSchema)
+def get_parking_data(request, sensor_id: int):
+    """Retrieve the latest parking data for a specific sensor."""
+    parking_data = (
+        ParkingData.objects.filter(sensor_id=sensor_id).order_by("-timestamp").first()
+    )
+    if not parking_data:
+        return {"detail": "Data not found"}, 404
+    return parking_data
--- smart_campus/parking_management/models.py
+++ smart_campus/parking_management/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class ParkingData(models.Model):
+    sensor_id = models.IntegerField()
+    sensor_location = models.CharField(max_length=300)
+    parking_status = models.BooleanField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"SensorData at {self.timestamp}"
