609e3be2fe9b31a2368f61c29689fd2dd22f1673
nerdparth <parthkapoor488@gmail.com>
2024-11-15 22:57:06 +0530

    added event_management

--- smart_campus/event_management/schemas.py
+++ smart_campus/event_management/schemas.py
@@ -1,2 +1,23 @@
 from ninja import ModelSchema, Schema
 from .models import Events, Attendees
+
+
+class AddEventSchema(ModelSchema):
+    class Meta:
+        model = Events
+        fields = "__all__"
+
+
+class DeleteEventSchema(Schema):
+    event_name: str
+
+
+class RegisterEventSchema(Schema):
+    event_name: str
+    name: str
+    email: str
+    semester: str
+
+class EventsCountSchema(Schema):
+    events_count : int
+    upcoming_events_count : int
